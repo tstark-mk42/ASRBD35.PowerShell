caa801959f3aaf7daad5b7d38f51bc80d0dc1f9b
tstark-mk42 <samiromar.zidat@gmail.com>
2018-05-28 16:02:39 +0200

    first commit

---
+++ cours.ps1
@@ -0,0 +1,71 @@
+﻿$fruits = @("Pomme", "Cerise", "Abricot")
+
+foreach ($item in $fruits) {
+  Write-Output $item ;
+}
+
+for ($i = 0 ; $i -lt 10 ; $i ++) {
+
+  Write-Output $i ;
+
+}
+
+$i = 0 ; 
+
+While ($i -lt 10){
+ 
+  Write-Output $i ;
+  $i ++ ; 
+}
+
+$i = 0 ;
+
+do {
+ 
+ Write-Output $i ;
+ $i ++ ; 
+} while ($i -lt 10)
+
+
+$i = 0 ; 
+
+do {
+Write-Output $i ;
+$i ++ ;
+} until ($i -eq 10)
+
+
+function disBonjour {
+
+ param($prenom) 
+
+ if ($prenom) { echo "Bonjour $prenom" } else { echo "Il a pas dit bonjour .. "}
+
+}
+
+disBonjour("Pierre")
+
+'''
+verb noun  option  value
+get-service -name  "*net*"
+
+'''
+class Car {
+
+  [String] $color
+  [String] $model
+  [int] $year = 1989
+
+  static [int] $wheels = 4
+
+  drive ($speed) {}
+
+  openRoof () {}
+
+}
+
+$twingo = New-Object Car
+$twingo.color = "Green"
+$twingo.model = "Twingo"
+$twingo.year = 2000
+$twingo
---
+++ exercices.ps1
@@ -0,0 +1,83 @@
+﻿###
+#### $msgOutput = Read-Host
+#### $i = 0 
+
+#### while ($i -lt 11) {
+
+#### echo len.$msgOutput
+#### $i ++
+#### }
+
+
+#### $a, $b, $c = 1
+ #### $i = 0
+
+#### while ( $i -lt 15000) { 
+  ####  $i ++
+  ####  echo $i
+ #### }
+
+
+#### function ex09 {
+
+ ####   for ($i = 1; $i -lt 15000; $i = $i + 2) {
+  ####    echo $i 
+####    }
+
+#### } 
+
+#### ex09
+
+
+function ex10 
+{
+    for ($i = 0 ; $i -lt 10 ; $i ++) {
+        echo $i*13
+    }
+}
+
+ex10
+
+function ex11 {
+    $word = read-Host "Donne un mot : "
+    
+
+    foreach($key in $word) {
+      echo $word.Length
+      
+    }
+    
+}
+
+ex11
+
+function ex12 {
+    [int] $userNum = Read-Host "Entrez un entier : "
+    
+    if ($userNum % 2 -ne 0) {
+        echo "Pas pair"
+    } else {
+        echo "est pair"
+    }
+}
+
+ex12
+
+function ex13 {
+    [int] $intervaleNum = Read-Host "Entrez un nombre compris entre 10 et 20 : "
+
+    if ($intervaleNum -lt 10) {
+        echo "Plus petit que 10"
+    } elseif ($intervaleNum -gt 20) {
+        echo "Plus grand que 20"
+    }
+}
+
+ex13
+
+function ex13b {
+    [int] $intervaleNum = Read-Host "Entrez un nombre compris entre 10 et 20 : "
+    while ($intervaleNum -lt 10 -or $intervaleNum -gt 20) {
+        echo "Error"
+    }
+}
